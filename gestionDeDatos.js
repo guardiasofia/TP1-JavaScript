dbf60da986f97662ae6dc6d3ec2e4400c99ae9f7
Luciano7786 <lucianocirvini7@gmail.com>
2025-08-11 21:55:17 -0300

    Ejercicios 3, 4 y 5

--- gestionDeDatos.js
+++ gestionDeDatos.js
@@ -3,8 +3,8 @@ console.log('-------------PUNTO 1.3-------------');
 const productos = [
   { id: 1, nombre: 'Leche entera', precio: 1200, stock: 25 },
   { id: 2, nombre: 'Pan lactal', precio: 950, stock: 40 },
-  { id: 3, nombre: 'Arroz 1kg', precio: 1300, stock: 18 },
-  { id: 4, nombre: 'Aceite de girasol', precio: 2500, stock: 12 },
+  { id: 3, nombre: 'Arroz 1kg', precio: 1300, stock: 0 },
+  { id: 4, nombre: 'Aceite de girasol', precio: 2500, stock: 0 },
   { id: 5, nombre: 'Fideos spaghetti', precio: 1100, stock: 30 }
 ]
 
@@ -35,9 +35,20 @@ productos.push(
   {id: 6, nombre: 'Mermelada', precio: 1800, stock: 3},
   {id: 7, nombre: 'Yerba', precio: 1300, stock: 8 }
 );
+console.log(productos)
 
 console.log('-------------PUNTO 4.2-------------');
-
 productos.pop();
+console.log(productos)
 
+console.log('-------------PUNTO 4.3-------------');
+productos.unshift({id: 0, nombre: 'Galletas Saladas', precio: 1300, stock: 70});
+console.log(productos)
 
+console.log('-------------PUNTO 4.4-------------');
+productos.shift()
+console.log(productos)
+
+console.log('-------------PUNTO 4.5-------------');
+const productosConStock = productos.filter(producto => producto.stock > 0);
+console.log(productosConStock)
